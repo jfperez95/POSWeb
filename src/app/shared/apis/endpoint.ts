50aaf978552f7e1362338ea7e82ff0ccd0360eb1
jfperez <jfperez9510@gmail.com>
2026-01-03 17:11:00 -0500

    Creación del proyecto

---
+++ src/app/shared/apis/endpoint.ts
@@ -0,0 +1,8 @@
+export const endpoint = {
+    //Category Module
+    LIST_CATEGORIES: 'Category',
+    LIST_SELECT_CATEGORIES: 'Category/Select',
+    CATEGORY_BY_ID: 'Category/',
+    CATEGORY_REGISTER: 'Category/Register/',
+    CATEGORY_REMOVE: 'Category/Remove/'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  environment: "local",
+  api: "https://localhost:44393/api/",
+  filenameCsv: "dev-pos"
+};
